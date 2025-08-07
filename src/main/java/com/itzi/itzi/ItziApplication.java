48575528554ed4d0b6fd10339e667c5dfc402ed2
onlyoneprivate <pllm098@naver.com>
2025-08-07 20:53:31 +0900

    ⚙️Chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/itzi/itzi/ItziApplication.java
@@ -0,0 +1,13 @@
+package com.itzi.itzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itzi/itzi/ItziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itzi.itzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
