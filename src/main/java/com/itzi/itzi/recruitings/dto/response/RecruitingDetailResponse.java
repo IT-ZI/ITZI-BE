b39c7d95374a1bcfaa10725092629d20df88e544
onlyoneprivate <pllm098@naver.com>
2025-08-17 18:14:27 +0900

    ✨Feat: 제휴모집글 단건 조회 응답 DTO 구현

---
+++ src/main/java/com/itzi/itzi/recruitings/dto/response/RecruitingDetailResponse.java
@@ -0,0 +1,46 @@
+package com.itzi.itzi.recruitings.dto.response;
+
+import com.itzi.itzi.posts.domain.Status;
+import com.itzi.itzi.posts.domain.Type;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecruitingDetailResponse {
+
+    private Long postId;
+    private Long userId;
+
+    private LocalDate exposureEndDate;
+    private Long bookmarkCount;
+
+    // 카테고리 추가 필요
+
+    private Type type;                  // RECRUITING, PROMOTION
+    private Status status;              // DRAFT, PUBLISHED
+
+    private String title;
+    private String postImageUrl;
+    private String target;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String benefit;
+    private String condition;
+
+    private boolean targetNegotiable;
+    private boolean periodNegotiable;
+    private boolean benefitNegotiable;
+    private boolean conditionNegotiable;
+
+    private String content;
+
+    // 작성자 정보 블럭 추가 필요
+
+}
