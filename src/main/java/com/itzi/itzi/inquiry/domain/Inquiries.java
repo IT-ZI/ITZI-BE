a39ce89003620fd12fde0f147d05690ea91ed21e
sujin <20241005@sungshin.ac.kr>
2025-08-20 23:06:18 +0900

    :recycle: Refactor: Partnership -> Inquiries로 파일명 변경(#23)

--- src/main/java/com/itzi/itzi/inquiry/domain/Inquiries.java
+++ src/main/java/com/itzi/itzi/inquiry/domain/Inquiries.java
@@ -11,7 +11,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class partnership {
+public class Inquiries {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
