993c600b021c07c1bc8228a0f02d2fb80296c865
onlyoneprivate <pllm098@naver.com>
2025-08-21 21:53:45 +0900

    ✨ Feat: 작성자·매장 요약 응답 DTO 추가

---
+++ src/main/java/com/itzi/itzi/recruitings/dto/response/StoreSummaryResponse.java
@@ -0,0 +1,30 @@
+package com.itzi.itzi.recruitings.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreSummaryResponse {
+
+    private String image;
+    private Integer rating;
+    private String name;
+    private String info;
+    private Set<String> keywords;
+
+    private String category;
+    private String operatingHours;
+    private String phone;
+    private String address;
+    private String ownerName;
+    private String linkUrl;
+
+
+}
