a1f8f732cadd1b612f3bc36194f1dc50fa2e9eb7
sujin <20241005@sungshin.ac.kr>
2025-08-24 03:57:41 +0900

    :recycle: Refactor: PartnershipPostRequestDTO postId 추가(#34)

--- src/main/java/com/itzi/itzi/partnership/dto/request/PartnershipPostRequestDTO.java
+++ src/main/java/com/itzi/itzi/partnership/dto/request/PartnershipPostRequestDTO.java
@@ -13,6 +13,8 @@ import java.util.Set;
 @Builder
 public class PartnershipPostRequestDTO {
     private Long receiverId;
+    private Long postId;   // ✅ 추가 (어떤 모집글에 대한 문의인지 반드시 필요)
+
     private String purpose;
     private PeriodType periodType;
     private String periodValue;
