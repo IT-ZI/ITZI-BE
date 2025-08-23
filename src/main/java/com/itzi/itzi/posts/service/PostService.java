a70ec8c3a02f12440268888e0368a1e617b92255
onlyoneprivate <pllm098@naver.com>
2025-08-23 17:41:55 +0900

    ♻️ Refactor: 게시물 업로드 공통 코드로 구현

--- src/main/java/com/itzi/itzi/posts/service/PostService.java
+++ src/main/java/com/itzi/itzi/posts/service/PostService.java
@@ -4,12 +4,16 @@ import com.itzi.itzi.global.api.code.ErrorStatus;
 import com.itzi.itzi.global.exception.GeneralException;
 import com.itzi.itzi.posts.domain.Post;
 import com.itzi.itzi.posts.domain.Status;
+import com.itzi.itzi.posts.domain.Type;
 import com.itzi.itzi.posts.dto.response.PostDeleteResponse;
 import com.itzi.itzi.posts.repository.PostRepository;
+import com.itzi.itzi.posts.dto.response.PostPublishResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -37,6 +41,45 @@ public class PostService {
                 post.getType(),
                 post.getPostId(),
                 post.getStatus()
+        );
+    }
+
+    // 제휴 홍보글 게시하기
+    @Transactional
+    public PostPublishResponse pusblishPost(Long postId) {
+
+        // 존재하는 게시글인지 확인
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.NOT_FOUND));
+
+        // 이미 게시된 글인지 확인
+        if (post.getStatus() == Status.PUBLISHED) {
+            throw new GeneralException(ErrorStatus.ALREADY_PUBLISHED);
+        }
+
+        // 제휴 모집글 게시를 위해서는 모든 필드가 작성돼야 함
+        if (post.getPostImage() == null ||
+                post.getTitle() == null || post.getTitle().isBlank() ||
+                post.getTarget() == null || post.getTarget().isBlank() ||
+                post.getStartDate() == null || post.getEndDate() == null ||
+                post.getBenefit() ==  null || post.getBenefit().isBlank() ||
+                post.getCondition() == null || post.getCondition().isBlank() ||
+                post.getContent() == null || post.getContent().isBlank() ||
+                post.getExposureEndDate() == null ) {
+            throw new GeneralException(ErrorStatus.REQUIRED_FIELD_MISSING);
+        }
+
+        // 게시 상태로 변경 및 생성 시간 업데이트
+        post.setStatus(Status.PUBLISHED);
+        post.setPublishedAt(LocalDateTime.now());
+
+        postRepository.save(post);
+
+        return new PostPublishResponse(
+                post.getType(),
+                post.getPostId(),
+                post.getStatus(),
+                post.getPublishedAt()
         );
     }
 }
