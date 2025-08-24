23b3056d17537ff1ffa0e41dc048f7293264ed8a
onlyoneprivate <pllm098@naver.com>
2025-08-25 03:54:39 +0900

    ✨ Feat: PostRepository에 JpaSpecificationExecutor 확장

--- src/main/java/com/itzi/itzi/posts/repository/PostRepository.java
+++ src/main/java/com/itzi/itzi/posts/repository/PostRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -16,7 +17,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
     // 내 글 + 타입 + 상태 필터
     List<Post> findByUser_UserIdAndTypeAndStatusIn(
